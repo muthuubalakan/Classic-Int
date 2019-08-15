7da08d3159fb9ab07155cff5890a8f73d08ce4de
Muthu Balakan <muthuumuthu@gmail.com>
2019-08-15 14:15:31 +0200

    Create classic_int.py

---
+++ classic_int.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+# coding: utf-8
+
+
+class ClassicInt(int):
+    def __init__(self, integer):
+        self.integer = integer
+        self.numbers = [int(i) for i in str(integer)]
+        self.__class__.__call__ = self.integer
+
+    def __repr__(self):
+        return f'{self.integer}'
+
+    def __len__(self):
+        return len(self.numbers)
+
+    def __iter__(self):
+        for number in self.numbers:
+            yield number
+
+    def __add__(self, others=None):
+        # if not others:
+        #     return sum(self.numbers)
+        return self.integer + others
+    
+    def __sum__(self, others=None):
+        if not others:
+            return sum(self.numbers)
+        return self.numbers + others
+    
+    def __getitem__(self, index):
+        return self.numbers[index]
+
+    def __bool__(self):
+        return bool(abs(self))
+    
+    def __reversed__(self):
+        return reversed(self.numbers)
+    
+    def as_list(self):
+        return self.numbers
+    
+    def index(self, value):
+        return self.numbers.index(value)
